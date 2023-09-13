ba388120cb6ed02a5bb30f7be76f5222b6251985
samuel <samfrosh673@gmail.com>
2023-09-13 18:30:58 +0100

    populated the product Category with real-time data

--- app/src/main/java/com/samfrosh/martmobile/fragment/Home.java
+++ app/src/main/java/com/samfrosh/martmobile/fragment/Home.java
@@ -9,9 +9,18 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
 import com.samfrosh.martmobile.adapter.ProductCategoryAdapter;
 import com.samfrosh.martmobile.databinding.FragmentHomeBinding;
 import com.samfrosh.martmobile.dto.ProductCategory;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +31,8 @@ public class Home extends Fragment {
     FragmentHomeBinding binding;
     RecyclerView recyclerView;
     ProductCategoryAdapter adapter;
+
+    private static final String PRODUCT_CATEGORY_API = "https://amused-tooth-production.up.railway.app/productstatus";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -35,20 +46,42 @@ public class Home extends Fragment {
         linearLayoutManager.setOrientation(RecyclerView.HORIZONTAL);
         recyclerView.setHasFixedSize(true);
 
-        adapter = new ProductCategoryAdapter(productCategoryList);
-        recyclerView.setAdapter(adapter);
 
-        loadItems();
+
+        loadProductCategoryItems();
 
 
         return binding.getRoot();
     }
 
-    private void loadItems() {
-        productCategoryList.add(new ProductCategory("Men"));
-        productCategoryList.add(new ProductCategory("Women"));
-        productCategoryList.add(new ProductCategory("Hats"));
-        productCategoryList.add(new ProductCategory("Bags"));
-        productCategoryList.add(new ProductCategory("Shoes"));
+    private void loadProductCategoryItems() {
+        RequestQueue requestQueue = Volley.newRequestQueue(getContext());
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, PRODUCT_CATEGORY_API, null, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                try {
+                    for (int i = 0; i < response.length(); i++) {
+                        JSONObject item  = response.getJSONObject(i);
+                        String categoryName = item.getString("statusName");
+                        System.out.println(categoryName);
+
+                        productCategoryList.add(new ProductCategory(categoryName));
+                    }
+                    adapter = new ProductCategoryAdapter(productCategoryList);
+                    recyclerView.setAdapter(adapter);
+
+                } catch (Exception e) {
+                    System.out.println("errr"+e);
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println("error"+ error);
+            }
+        });
+
+        requestQueue.add(jsonArrayRequest);
     }
 }
